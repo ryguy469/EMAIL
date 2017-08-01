ac87aa7bf9499da2dd9854f714d34b0935b9b6bb
Ryan Deegan <rdd299@nyu.edu>
2017-07-31 21:28:52 -0400

    added weather for the beach

--- email_me.py
+++ email_me.py
@@ -23,10 +23,16 @@ sg = sendgrid.SendGridAPIClient(apikey = SENDGRID_API_KEY)
 baseurl = "https://query.yahooapis.com/v1/public/yql?"
 yql_query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='new york, ny')"
 yql_url = baseurl + urllib.parse.urlencode({'q':yql_query}) + "&format=json"
+baseurl = "https://query.yahooapis.com/v1/public/yql?"
+yql_query_nj = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='beach haven, nj')"
+yql_url_nj = baseurl + urllib.parse.urlencode({'q':yql_query_nj}) + "&format=json"
+
+
 
 # ISSUE REQUEST
 
 response = urllib.request.urlopen(yql_url).read()
+response_nj = urllib.request.urlopen(yql_url_nj).read()
 
 # PARSE RESPONSE
 
@@ -35,19 +41,20 @@ raw_response = json.loads(response)
 results = raw_response["query"]["results"]["channel"]
 weather = results["item"]
 
-#forecast = []
-
-#for f in weather:
-#    weather += "Today's forecast is: " + f["date"] + f["text"])
+raw_response_nj = json.loads(response_nj)
+results_nj = raw_response_nj["query"]["results"]["channel"]
+weather_nj = results_nj["item"]
 
 print(weather["title"])
 print(weather["condition"]["temp"])
 print(weather["condition"]["text"])
-
+print(weather_nj["title"])
+print(weather_nj["condition"]["temp"])
+print(weather_nj["condition"]["text"])
 
 #print(weather)
 
-# COMPILE REQUEST PARAMETERS
+#COMPILE REQUEST PARAMETERS
 
 #email_message = str(weather)
 
